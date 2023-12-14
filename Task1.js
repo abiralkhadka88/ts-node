fc9f346c667f2f7578049164fc4e27c9aa23a1ff
Abiral <abiralkhadka88@gmail.com>
2023-12-14 17:58:07 +0545

    Transpile to js

--- Task1.js
+++ Task1.js
@@ -29,3 +29,69 @@ studentsObjectList.push({
     school: "Himsagar Higher secondary school",
     id: "123-M2",
 });
+/* difference between unknown and any
+
+any=> takes anything
+unknown=> thaha nabhayey samma operation garna mildaina
+
+
+
+*/
+//Never : error throw garda use garney , never going to occur
+var ROLES;
+(function (ROLES) {
+    ROLES[ROLES["ADMIN"] = 0] = "ADMIN";
+    ROLES[ROLES["SUPERADMIN"] = 1] = "SUPERADMIN";
+    ROLES[ROLES["USER"] = 2] = "USER";
+})(ROLES || (ROLES = {}));
+console.log(ROLES.ADMIN); //PRINTS 0
+//special class that represents a group of constants
+var ERROR;
+(function (ERROR) {
+    ERROR[ERROR["NotFound"] = 404] = "NotFound";
+    ERROR[ERROR["Unauthorized"] = 401] = "Unauthorized";
+    ERROR[ERROR["BadRequest"] = 400] = "BadRequest";
+})(ERROR || (ERROR = {}));
+//static is used for memory optimization, shares memory between class objects
+var Util = /** @class */ (function () {
+    function Util() {
+    }
+    Util.getResult = function (num) {
+        return num * this.MULTIPLIER;
+    };
+    Util.MULTIPLIER = 13;
+    return Util;
+}());
+console.log(Util.MULTIPLIER);
+console.log(Util.getResult(5));
+//generics
+//statically typed programming language ma matra auchha
+//reduces code repitition
+//funtion ko lagi , class ko lagi,
+function fun(args) {
+    return args;
+}
+var result1 = fun("Hello World");
+var result2 = fun(200);
+console.log(result1);
+console.log(result2);
+var salary = { annual: 56000, bonus: 1200 };
+//Record<Key,value>  ==> is a generic type
+var salary1 = { annual: 56000, bonus: 1200 };
+var salary2 = { annual: 56000, bonus: 1200 };
+function logSalary1(salary) {
+    console.log(salary);
+}
+function logSalary2(salary) {
+    console.log(salary);
+}
+// this is equivalent to a type below
+// type TechTeam = {
+//  id: string;
+//  members: { name: string, email: string, age: string }[]
+// };
+//Partial types make everthing partial
+var InternTechTeam = {
+    id: "techIntern11",
+    name: "hari",
+};
